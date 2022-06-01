19e6aed6cf6fa1a267cfd1cf40daf5419696de64
ZimTang <2351061127@qq.com>
2022-06-01 17:32:32 +0800

    feat:linklist,LRU,位运算

--- 数据结构与算法/前端啃算法笔记/source/LinkList/LinkList.js
+++ 数据结构与算法/前端啃算法笔记/source/LinkList/LinkList.js
@@ -42,12 +42,29 @@ class LinkList{
 
 }
 
-const linkList = new LinkList()
+// const linkList = new LinkList()
 
-linkList.append(1)
-linkList.append(2)
-linkList.append(3)
-linkList.append(4)
+// linkList.append(1)
+// linkList.append(2)
+// linkList.append(3)
+// linkList.append(4)
 
-linkList.print()
-console.log(linkList.length)
+// linkList.print()
+// console.log(linkList.length)
+
+
+// let cache = new Map()
+// cache.set('a',1)
+// cache.set('b',2)
+// cache.set('c',3)
+// cache.delete(cache.keys().next().value)
+
+
+// size:4
+// 1=>2=>3=>4
+// 加入
+// 没有出现过
+// 6=>1=>2=>3
+// 出现过
+// 3=>1=>2=>4
+
