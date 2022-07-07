b60ec2e94308abea7ffb4a56370b687e77db252b
ZimTang <2351061127@qq.com>
2022-07-07 14:36:48 +0800

    feat: 151 and 1047

---
+++ 数据结构与算法/前端啃算法笔记/source/code/151.颠倒字符串中的单词.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=javascript
+ *
+ * [151] 颠倒字符串中的单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  let queue = [];
+  let word = "";
+  while (s.charAt(left) === " ") {
+    left++;
+  }
+  while (s.charAt(right) === " ") {
+    right--;
+  }
+
+  while (left <= right) {
+    let ch = s.charAt(left);
+    if (ch == " " && word) {
+      queue.unshift(word);
+      word = "";
+    } else if (ch !== " ") {
+      word += ch;
+    }
+    left++;
+  }
+  queue.unshift(word);
+  return queue.join(" ");
+};
+// @lc code=end
